a5ec7ae2a1d1eb1379de0693d544f6f120fec347
nikandfilat <nikandfilat@gmail.com>
2026-04-19 20:37:58 +0300

    Update products.js

--- products.js
+++ products.js
@@ -45,8 +45,8 @@ const ICONS = {
 const PRODUCT_PHOTOS = {
   1: "https://cdn2.divan.ru/img/v1/_oVejLxiGe3QfvKehOKdAs6K8JC2oyU0QC0GIeVxEfc/pd:0:0:0:0/rs:fill:1268:840:1:1:ce:0:0/g:ce:0:0/bg:f5f3f1/q:85/czM6Ly9kaXZhbi9wcm9kdWN0LzQxNzUwMDcuanBn.jpg",
   2: "https://avatars.mds.yandex.net/get-mpic/15250760/2a00000198341a5c6a40809f268d01a88588/orig",
-  3: "https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?auto=format&fit=crop&w=900&q=85",
-  4: "https://images.unsplash.com/photo-1506439773649-6e0eb8cfb237?auto=format&fit=crop&w=900&q=85",
+  3: "https://lifemebel.ru/photo/stoly/dlya_kuhni/derevyannye/derevyannyy_stol_miramar_mdi_dt_mrm_14_shpon_orekha_f_chernyy_matovyy_lak_240kh109kh75/derevyannyy_stol_miramar_mdi_dt_mrm_14_shpon_orekha_f_chernyy_matovyy_lak_240kh109kh75_997_660_1_0_5253168.jpg",
+  4: "https://ir.ozone.ru/s3/multimedia-1-c/7066333812.jpg",
   5: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?auto=format&fit=crop&w=900&q=85",
   6: "https://images.unsplash.com/photo-1618220179428-22790b461013?auto=format&fit=crop&w=900&q=85",
   7: "https://images.unsplash.com/photo-1540574163026-643ea20ade25?auto=format&fit=crop&w=900&q=85",
